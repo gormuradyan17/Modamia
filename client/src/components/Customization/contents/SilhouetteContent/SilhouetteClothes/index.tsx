3715c1e24f9545a499a97827f9c6bc0a397930c0
Aalexey01 <aalexafanasev@gmail.com>
2023-12-12 11:52:33 +0400

    fixed styles on canvas

--- client/src/components/Customization/contents/SilhouetteContent/SilhouetteClothes/index.tsx
+++ client/src/components/Customization/contents/SilhouetteContent/SilhouetteClothes/index.tsx
@@ -15,7 +15,7 @@ export default function SilhouetteClothes({ data, type }: any) {
   const isLoading = useSelector(getMannequinLoading)
   const settings = {
     dots: false,
-    infinite: true,
+    infinite: false,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
